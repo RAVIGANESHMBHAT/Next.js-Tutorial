c91247c70270e25fec95a9845aa7a248e8f8484e
Raviganesh M <raviganeshmbhat999@gmail.com>
2024-02-25 17:32:52 +0530

    Static Routing Metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
+
+export const metadata = {
+  title: "About Project",
+};
 
 function About() {
-  return (
-    <div>About</div>
-  )
+  return <div>About</div>;
 }
 
-export default About
+export default About;
