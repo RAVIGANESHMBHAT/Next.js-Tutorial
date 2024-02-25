1a601cb4350386eb015008bb9697b627142b0cc5
Raviganesh M <raviganeshmbhat999@gmail.com>
2024-02-25 17:50:38 +0530

    title Metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: {
+    absolute: "Blog",
+  },
+};
 
 function Blog() {
-  return (
-    <div>Blogs Page</div>
-  )
+  return <div>Blogs Page</div>;
 }
 
-export default Blog
+export default Blog;
